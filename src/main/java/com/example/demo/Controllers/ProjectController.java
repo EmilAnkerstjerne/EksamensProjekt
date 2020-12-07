b46c9dbb772348fa95ba20a2b1efb81ef756798e
Anker-PC <tobias.as@hotmail.com>
2020-12-07 11:17:09 +0100

    project-overview connected with controller - added a test endpoint /startside2

--- src/main/java/com/example/demo/Controllers/ProjectController.java
+++ src/main/java/com/example/demo/Controllers/ProjectController.java
@@ -16,6 +16,7 @@ public class ProjectController {
     ProfileService profileService = new ProfileService();
     ProjectService projectService = new ProjectService();
 
+    //more or less done TODO: invitations in navbar
     @GetMapping("/startside")
     public String startside(ModelMap modelMap, @CookieValue(value = "user", defaultValue = "") String cookie){
         int profileID = Login.verifyCookie(cookie);
@@ -26,8 +27,23 @@ public class ProjectController {
         projectService.getAdminProjects(profileID,modelMap,false);
         projectService.getOtherProjects(profileID, modelMap, false);
         modelMap.addAttribute("invitations", profileService.getInvitations(profileID));
+        return "project-overview";
+    }
+
+    //Testing
+    @GetMapping("/startside2")
+    public String startsideTwo(ModelMap modelMap, @CookieValue(value = "user", defaultValue = "") String cookie){
+        int profileID = Login.verifyCookie(cookie);
+        if(profileID == -1){
+            return "redirect:/login";
+        }
+        modelMap.addAttribute("user",profileID);
+        projectService.getAdminProjects(profileID,modelMap,false);
+        projectService.getOtherProjects(profileID, modelMap, false);
+        modelMap.addAttribute("invitations", profileService.getInvitations(profileID));
         return "startside";
     }
+
 
     //JOHN
     @GetMapping("/projektArkiv")
@@ -49,6 +65,8 @@ public class ProjectController {
         if(profileID == -1){
             return "redirect:/login";
         }
+
+
         int projectID = Integer.parseInt(request.getParameter("projectID"));
         modelMap.addAttribute("project", projectService.getProject(projectID));
         return "test-project-summary-page";
