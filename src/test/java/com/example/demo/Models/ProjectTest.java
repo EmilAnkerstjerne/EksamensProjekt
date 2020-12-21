c41a896588b76f8112cd5a08b972404452fc423a
Anker-PC <tobias.as@hotmail.com>
2020-12-21 04:43:14 +0100

    JUnit tests + changes in insight.html

--- src/test/java/com/example/demo/Models/ProjectTest.java
+++ src/test/java/com/example/demo/Models/ProjectTest.java
@@ -39,7 +39,7 @@ class ProjectTest {
         Assertions.assertEquals(result,p1.calculateDeadline());
     }
 
-
+    //TOBIAS
     @Test
     void getTotalHoursAvailableTest() {
         String pattern = "dd/MM/yyyy";
@@ -58,6 +58,58 @@ class ProjectTest {
 
         int result = 10;
         Assertions.assertEquals(result,p1.getTotalHoursAvailable());
+
+    }
+
+    //TOBIAS
+    @Test
+    void calculateHoursWeekEmployeeNeededTest() {
+        String pattern = "dd/MM/yyyy";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
+        Date startdate = null;
+        Date deadline = null;
+        try {
+            startdate = simpleDateFormat.parse("21/12/2020");
+            deadline = simpleDateFormat.parse("28/12/2020");
+        } catch (ParseException | NullPointerException e) {
+            System.out.println(e);
+        }
+
+        Project p1 = new Project(1,"Test projekt",1, deadline,startdate,
+                0,1,0,false,1);
+
+        Subproject sp1 = new Subproject(1,1,"test Sub1");
+        p1.addSubproject(sp1);
+
+        Task t1 = new Task(1,1,"test task1");
+        sp1.addTask(t1);
+
+        Subtask st1 = new Subtask(1,1,"test subtask1",20);
+        t1.addSubtask(st1);
+
+        int result = 20;
+        Assertions.assertEquals(result,p1.calculateHoursWeekEmployeeNeeded());
+    }
+
+    //TOBIAS
+    @Test
+    void getTotalSubprojectsTime() {
+        Project p1 = new Project(1,"Test projekt",1, null,null,
+                0,0,0,false,0);
+
+        Subproject sp1 = new Subproject(1,1,"test Sub1");
+        p1.addSubproject(sp1);
+
+        Task t1 = new Task(1,1,"test task1");
+        sp1.addTask(t1);
+
+        Subtask st1 = new Subtask(1,1,"test subtask1",20);
+        Subtask st2 = new Subtask(2,1,"test subtask1",30);
+        t1.addSubtask(st1);
+        t1.addSubtask(st2);
+
+        int result = 50;
+        Assertions.assertEquals(result,p1.getTotalSubprojectsTime());
 
     }
 }
